f943dfa7e9bb39150c0a2d309e114be1d58b4c35
Katie <kpoa@berkeley.edu>
2020-10-08 12:21:01 -0700

    Fix endpoints

--- proxy/server/index.js
+++ proxy/server/index.js
@@ -12,16 +12,20 @@ app.use(cors());
 app.use('/', express.static(path.join(__dirname, '../client/dist')));
 
 // Gallery
-app.use('/gallery/', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
+app.use('/gallery/stays/:roomId', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
+// separate /gallery/lists endpoint
+app.use('/gallery/list', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
 
 // Booking
 app.use('/booking/', createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin: true }));
 
 // Reviews
-app.use('/reviews/', createProxyMiddleware({ target: 'http://localhost:3003', changeOrigin: true }));
+app.use('/reviews/stays', createProxyMiddleware({ target: 'http://localhost:3003', changeOrigin: true }));
 
 // More Places
-app.use('/more-places/', createProxyMiddleware({ target: 'http://localhost:3004', changeOrigin: true }));
+app.use('/more-places/stays', createProxyMiddleware({ target: 'http://localhost:3004', changeOrigin: true }));
+// separate /more-places/lists endpoint
+app.use('/more-places/favorites', createProxyMiddleware({ target: 'http://localhost:3004', changeOrigin: true }));
 
 app.listen(port, () => {
   console.log(`Keybox running at: http://localhost:${port}`);
